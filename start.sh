406380cd8db8dd98663f2dd475dede4042c4c3c2
Samantha Clotfelter <samclot@protonmail.com>
2017-07-13 21:42:28 -0400

    Added power settings

--- start.sh
+++ start.sh
@@ -20,6 +20,11 @@ bash-completion \
 # mac-like window buttons
 xfconf-query -c xfwm4 -p /general/button_layout -s 'CHM|O'
 
+# power
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-battery -s 0
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-ac -s 0
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/show-tray-icon -s 1
+
 # set up padlock.io
 # configure settings
 
