347f67e5c967f18cad2ca74832cb41b63908a54d
Samantha Clotfelter <samclot@protonmail.com>
2018-03-26 19:16:25 -0400

    Added owncloud encrypted password

--- owncloud/sync.sh
+++ owncloud/sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-password=$(echo ~/.myconfig/owncloud/password.txt)
+password=$(echo ~/.myconfig/owncloud/password)
 owncloudcmd -u 1077 -p $password --exclude ~/.myconfig/exclude.txt ~/ownCloud/ https://nc3.jaba.hosting/remote.php/webdav
