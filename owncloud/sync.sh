b13f4bbfcb36e501db4cf8bc5b718811c0d59cef
Samantha Clotfelter <samclot@protonmail.com>
2018-03-26 20:43:39 -0400

    Fixed bugs in owncloud sync

--- owncloud/sync.sh
+++ owncloud/sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-password=$(echo ~/.myconfig/owncloud/password)
+password=$(cat ~/.myconfig/owncloud/password | xargs)
 if ! pgrep -x "owncloudcmd" > /dev/null
 then
-	owncloudcmd -u 1077 -p $password --exclude ~/.myconfig/exclude.txt ~/ownCloud/ https://nc3.jaba.hosting/remote.php/webdav
+	owncloudcmd -u 1077 -p $password --exclude ~/.myconfig/owncloud/exclude.txt ~/ownCloud/ https://nc3.jaba.hosting/remote.php/webdav
 else
 	echo 'Owncloudcmd is already running. Doing nothing.'
 fi
