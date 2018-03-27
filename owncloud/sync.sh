cbfbcadf075a8ec188e0bfc2a78483d51fd157ad
Samantha Clotfelter <samclot@protonmail.com>
2018-03-26 22:21:51 -0400

    minor change

--- owncloud/sync.sh
+++ owncloud/sync.sh
@@ -7,7 +7,7 @@ password=$(cat ~/.myconfig/owncloud/password | xargs)
 if ! pgrep -x "owncloudcmd" > /dev/null
 then
 	echo Running owncloudcmd. >> $log
-	owncloudcmd -u 1077 -p $password --exclude ~/.myconfig/owncloud/exclude.txt ~/ownCloud/ https://nc3.jaba.hosting/remote.php/webdav
+	owncloudcmd -u 1077 -p $password --exclude ~/.myconfig/owncloud/exclude.txt ~/ownCloud/ 'https://nc3.jaba.hosting/remote.php/webdav'
 else
 	echo 'owncloudcmd is already running. Doing nothing.' >> $log
 fi
