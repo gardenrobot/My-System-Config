0703c7c67b43fdcd6d2cc91d6f19e67a58f38c9a
Samantha Clotfelter <samclot@protonmail.com>
2018-05-04 18:30:57 -0400

    Added configurations and modified ansible for neomutt setup

--- chromebook/login.sh
+++ chromebook/login.sh
@@ -9,3 +9,4 @@ python ~/.reddit-search-notifications/reddit-search-notifications.py &
 tint2 -c ~/.myconfig/tint2rc &
 syncthing -no-browser &
 sshfs kuruk:/home/sam/Shared/ /home/sam/Shared/ -o ro &
+~/.myconfig/tmux-start
