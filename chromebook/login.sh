ff91f6117d664d3dd798a8637f1e3e4de6e2ebff
Samantha Clotfelter <samclot@protonmail.com>
2018-05-05 06:43:47 -0400

    reconnect sshfs after suspend

--- chromebook/login.sh
+++ chromebook/login.sh
@@ -8,5 +8,5 @@ discord-ptb &
 python ~/.reddit-search-notifications/reddit-search-notifications.py &
 tint2 -c ~/.myconfig/tint2rc &
 syncthing -no-browser &
-sshfs kuruk:/home/sam/Shared/ /home/sam/Shared/ -o ro &
+sshfs kuruk:/home/sam/Shared/ /home/sam/Shared/ -o ro,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 &
 ~/.myconfig/tmux-start
