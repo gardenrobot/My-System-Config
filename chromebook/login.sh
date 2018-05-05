79c56f27f1057058b64678719567baa1a53eb162
Samantha Clotfelter <samclot@protonmail.com>
2018-05-05 08:50:21 -0400

    Moved offlineimap to background. Renamed tmux window. Remove bash tmux window.

--- chromebook/login.sh
+++ chromebook/login.sh
@@ -9,4 +9,5 @@ python ~/.reddit-search-notifications/reddit-search-notifications.py &
 tint2 -c ~/.myconfig/tint2rc &
 syncthing -no-browser &
 sshfs kuruk:/home/sam/Shared/ /home/sam/Shared/ -o ro,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 &
+offlineimap &
 ~/.myconfig/tmux-start
