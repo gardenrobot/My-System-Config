8a807513a044a16720310746ecc43dafbb5b8d82
Samantha Clotfelter <samclot@protonmail.com>
2018-05-22 00:31:23 -0400

    ~/.reminders is generated regularly with caldav data

--- calendar-sync.sh
+++ calendar-sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 vdirsyncer discover calendar_personal
+vdirsyncer discover calendar_personal_single
 vdirsyncer discover contacts_personal
 
 sleep=1200
@@ -9,6 +10,8 @@ while [ 1 ]
 do
 	echo 'Syncing'
 	vdirsyncer sync
+	echo 'Running ics2rem'
+	ics2rem ~/.reminder-cal/personal.ics ~/.reminders
 	echo "Done. Sleeping for $sleep seconds."
 	sleep $sleep
 done
